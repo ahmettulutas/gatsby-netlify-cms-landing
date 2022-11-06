14b277067566da8412fa05e0a6c7339f3056d28b
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-11-06 16:35:55 +0300

    removed conflicts

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,7 +4,6 @@ import mail from '../img/social/mail.svg';
 import phone from '../img/social/phone.svg';
 
 const ContactInfo = () => {
-<<<<<<< Updated upstream
 	return (
 		<article className='contact-main column is-12-mobile is-4-desktop is-flex is-justify-content-center'>
 			<section className='columns contact-card'>
@@ -35,39 +34,5 @@ const ContactInfo = () => {
 			</section>
 		</article>
 	);
-=======
-		return (
-			<article className='contact-main column is-12-mobile is-12-desktop'>
-				<section className='columns contact-card'>
-					<div className='column is-12-mobile'>
-						<span className='icon is-large'>
-							<img src={phone} alt='instagram link' />
-						</span>
-						<p className='is-size-5-media'>+90 530 095 40 85</p>
-					</div>
-					<a
-						className='column is-12-tablet'
-						href='https://www.instagram.com/psikologmeltemulutas/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						<span className='icon is-large'>
-							<img src={instagram} alt='instagram link' />
-						</span>
-						<p className='is-size-5-media'>
-							instagram.com/psikologmeltemulutas
-						</p>
-					</a>
-					<a
-						className='column is-12-tablet'
-						href='mailto:psikolog.meltem.ulutas@gmail.com'>
-						<span className='icon is-large'>
-							<img src={mail} alt='instagram link' />
-						</span>
-						<p className='is-size-5-media'>psikolog.meltem.ulutas@gmail.com</p>
-					</a>
-				</section>
-			</article>
-		);
->>>>>>> Stashed changes
 };
 export default ContactInfo;
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -39,8 +39,7 @@ export default class Index extends React.Component {
 		return (
 			<Layout>
 				<Hero title={'İletişim'} background={contact} hasDarkBg />
-<<<<<<< Updated upstream
-{/* 				<section className='section'>
+				{/* 				<section className='section'>
 					<div className='container'>
 						<div className='content'>
 							<h1>İletişim</h1>
@@ -102,101 +101,6 @@ export default class Index extends React.Component {
 											id={'message'}
 											required={true}
 										/>
-=======
-				<div className='hero-body'>
-					<div className='container p-auto'>
-						<div className='columns is-8 is-variable '>
-							<div className='column is-half has-text-left'>
-								<h1 className='title is-1'>İletişime Geçin</h1>
-								<div className='social-media'>
-									<a
-										href='https://facebook.com'
-										target='_blank'
-										className='button is-light is-large'>
-										<i className='fa fa-phone-square' aria-hidden='true'></i>
-									</a>
-									<a
-										href='https://instagram.com'
-										target='_blank'
-										className='button is-light is-large'>
-										<i className='fa fa-instagram' aria-hidden='true'></i>
-									</a>
-									<a
-										href='https://twitter.com'
-										target='_blank'
-										className='button is-light is-large'>
-										<i className='fa fa-whatsapp' aria-hidden='true'></i>
-									</a>
-								</div>
-							</div>
-							<div className='column is-half has-text-left'>
-								<form
-									name='contact'
-									method='post'
-									action='/contact/thanks/'
-									data-netlify='true'
-									data-netlify-honeypot='bot-field'
-									data-netlify-recaptcha='true'
-									onSubmit={this.handleSubmit}>
-									{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-									<input type='hidden' name='form-name' value='contact' />
-									<div hidden>
-										<label>
-											Don’t fill this out:{' '}
-											<input name='bot-field' onChange={this.handleChange} />
-										</label>
-									</div>
-									<div className='field'>
-										<label className='label' htmlFor='name'>
-											İsminiz
-										</label>
-										<div className='control'>
-											<input
-												className='input is-medium'
-												type='text'
-												required={true}
-												onChange={this.handleChange}
-												name='name'
-												id={'name'}
-											/>
-										</div>
-									</div>
-									<div className='field'>
-										<label className='label' htmlFor='email'>
-											Email Adresiniz
-										</label>
-										<div className='control'>
-											<input
-												className='input is-medium'
-												type='text'
-												required={true}
-												onChange={this.handleChange}
-												name='email'
-												id={'email'}
-											/>
-										</div>
-									</div>
-									<div className='field'>
-										<label className='label' htmlFor='message'>
-											Mesajınız
-										</label>
-										<div className='control'>
-											<textarea
-												className='textarea is-medium'
-												required={true}
-												onChange={this.handleChange}
-												name='message'
-												id={'message'}></textarea>
-										</div>
-									</div>
-									<div data-netlify-recaptcha='true'></div>
-									<div className='control'>
-										<button
-											type='submit'
-											className='button is-link is-fullwidth has-text-weight-medium is-medium'>
-											Mesaj Yolla
-										</button>
->>>>>>> Stashed changes
 									</div>
 								</div>
 								<div className='field'>
@@ -208,47 +112,69 @@ export default class Index extends React.Component {
 						</div>
 					</div>
 				</section> */}
-				<section class="hero is-fullheight">
-		<div class="hero-body">
-			<div class="container p-auto">
-				<div class="columns is-8 is-variable ">
-					<div class="column is-two-thirds has-text-left">
-						<h1 class="title is-1">Contact Us</h1>
-						<p class="is-size-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eligendi soluta
-							voluptate facere molestiae consequatur.</p>
-						<div class="social-media">
-							<a href="https://facebook.com" target="_blank" class="button is-light is-large"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-							<a href="https://instagram.com" target="_blank" class="button is-light is-large"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-							<a href="https://twitter.com" target="_blank" class="button is-light is-large"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<section class='hero is-fullheight'>
+					<div class='hero-body'>
+						<div class='container p-auto'>
+							<div class='columns is-8 is-variable '>
+								<div class='column is-two-thirds has-text-left'>
+									<h1 class='title is-1'>Contact Us</h1>
+									<p class='is-size-4'>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+										Nulla eligendi soluta voluptate facere molestiae
+										consequatur.
+									</p>
+									<div class='social-media'>
+										<a
+											href='https://facebook.com'
+											target='_blank'
+											class='button is-light is-large'>
+											<i class='fa fa-facebook-square' aria-hidden='true'></i>
+										</a>
+										<a
+											href='https://instagram.com'
+											target='_blank'
+											class='button is-light is-large'>
+											<i class='fa fa-instagram' aria-hidden='true'></i>
+										</a>
+										<a
+											href='https://twitter.com'
+											target='_blank'
+											class='button is-light is-large'>
+											<i class='fa fa-twitter' aria-hidden='true'></i>
+										</a>
+									</div>
+								</div>
+								<div class='column is-one-third has-text-left'>
+									<div class='field'>
+										<label class='label'>Name</label>
+										<div class='control'>
+											<input class='input is-medium' type='text' />
+										</div>
+									</div>
+									<div class='field'>
+										<label class='label'>Email</label>
+										<div class='control'>
+											<input class='input is-medium' type='text' />
+										</div>
+									</div>
+									<div class='field'>
+										<label class='label'>Message</label>
+										<div class='control'>
+											<textarea class='textarea is-medium'></textarea>
+										</div>
+									</div>
+									<div class='control'>
+										<button
+											type='submit'
+											class='button is-link is-fullwidth has-text-weight-medium is-medium'>
+											Send Message
+										</button>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="column is-one-third has-text-left">
-						<div class="field">
-							<label class="label">Name</label>
-							<div class="control">
-								<input class="input is-medium" type="text" />
-							</div>
-						</div>
-						<div class="field">
-							<label class="label">Email</label>
-							<div class="control">
-								<input class="input is-medium" type="text" />
-							</div>
-						</div>
-						<div class="field">
-							<label class="label">Message</label>
-							<div class="control">
-								<textarea class="textarea is-medium"></textarea>
-							</div>
-						</div>
-						<div class="control">
-							<button type="submit" class="button is-link is-fullwidth has-text-weight-medium is-medium">Send Message</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+				</section>
 			</Layout>
 		);
 	}
