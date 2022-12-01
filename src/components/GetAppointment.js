b5dce2ecdcbda792e2b7adb530e68a8d2ed39a08
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-12-01 19:17:37 +0300

    bugfix: getappointment text-center error

--- src/components/GetAppointment.js
+++ src/components/GetAppointment.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const GetAppointment = () => (
   <div className="get-appointment">
-    <h3>Psikolog randevunuzu oluşturmak için iletişim bilgilerinizi bırakırsanız sizi arayabiliriz.</h3>
-    <Link className='navbar-item' to='/getappointment'>RANDEVU TALEBİ OLUŞTURMAK İÇİN TIKLAYINIZ</Link>
+    <h3 className='has-text-centered'>Psikolog randevunuzu oluşturmak için iletişim bilgilerinizi bırakırsanız sizi arayabiliriz.</h3>
+    <Link className='navbar-item has-text-centered' to='/getappointment'>RANDEVU TALEBİ OLUŞTURMAK İÇİN TIKLAYINIZ</Link>
   </div>
 );
