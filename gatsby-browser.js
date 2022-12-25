397df5514648c25740f7faa42c1cd3b51e7219a2
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-12-25 14:05:26 +0300

    window.onload test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 export const onInitialClientRender = () => {
   window.addEventListener('load', () => {
     window.alert('content loaded');
+    document.getElementById('___loader').innerHTML = 'hello';
     document.getElementById('___loader').style.display = 'none';
   });
   document.addEventListener('DOMContentLoaded', () => {
     window.alert('dom loaded');
+    document.getElementById('___loader').innerHTML = 'hello';
     document.getElementById('___loader').style.display = 'none';
   });
 };
