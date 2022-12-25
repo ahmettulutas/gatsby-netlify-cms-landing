b2e9c0c024bfb24bb8960852eac8455ba2c6473e
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-12-25 13:52:26 +0300

    window.onload test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,41 @@ export const onInitialClientRender = () => {
   window.addEventListener('load', () => {
     setTimeout(() => { document.getElementById('___loader').style.display = 'none'; }, 100);
   });
+  const resourcePromises = [],
+
+    images = document.querySelectorAll('img');
+
+  images.forEach(img => {
+    const promise = new Promise((resolve, reject) => {
+      img.addEventListener('load', () => {
+        resolve();
+      });
+      img.addEventListener('error', () => {
+        reject();
+      });
+    });
+    resourcePromises.push(promise);
+  });
+
+  const scripts = document.querySelectorAll('script');
+
+  scripts.forEach(script => {
+    const promise = new Promise((resolve, reject) => {
+      script.addEventListener('load', () => {
+        resolve();
+      });
+      script.addEventListener('error', () => {
+        reject();
+      });
+    });
+    resourcePromises.push(promise);
+  });
+
+  Promise.all(resourcePromises).then(() => {
+  // All resources have finished loading
+    console.log('All resources have finished loading');
+
+    // Hide the loading spinner
+    document.getElementById('___loader').style.display = 'none';
+  });
 };
