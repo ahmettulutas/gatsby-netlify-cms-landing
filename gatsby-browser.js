39b8dbf44ee153661917f275d48bd1b7aa1d43e7
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-12-25 13:57:30 +0300

    window.onload test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,42 +1,10 @@
 export const onInitialClientRender = () => {
   window.addEventListener('load', () => {
-    setTimeout(() => { document.getElementById('___loader').style.display = 'none'; }, 100);
+    window.alert('content loaded');
+    document.getElementById('___loader').style.display = 'none';
   });
-  const resourcePromises = [],
-
-    images = document.querySelectorAll('img');
-
-  images.forEach(img => {
-    const promise = new Promise((resolve, reject) => {
-      img.addEventListener('load', () => {
-        resolve();
-      });
-      img.addEventListener('error', () => {
-        reject();
-      });
-    });
-    resourcePromises.push(promise);
-  });
-
-  const scripts = document.querySelectorAll('script');
-
-  scripts.forEach(script => {
-    const promise = new Promise((resolve, reject) => {
-      script.addEventListener('load', () => {
-        resolve();
-      });
-      script.addEventListener('error', () => {
-        reject();
-      });
-    });
-    resourcePromises.push(promise);
-  });
-
-  Promise.all(resourcePromises).then(() => {
-  // All resources have finished loading
-    console.log('All resources have finished loading');
-
-    // Hide the loading spinner
+  document.addEventListener('DOMContentLoaded', () => {
+    window.alert('dom loaded');
     document.getElementById('___loader').style.display = 'none';
   });
 };
