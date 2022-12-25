76e94ff36449c5ecce71855319a5fadfd67b0d31
ahmetulutas <ahmet.ulutas@ecozum.com>
2022-12-25 15:31:18 +0300

    bugfix: removed the code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,3 @@
-/*  export const onInitialClientRender = () => {
-  window.addEventListener('load', () => {
-    window.alert('content loaded');
-    document.getElementById('___loader').innerHTML = 'hello';
-    document.getElementById('___loader').style.display = 'none';
-  });
-  document.addEventListener('DOMContentLoaded', () => {
-    window.alert('dom loaded');
-    document.getElementById('___loader').innerHTML = 'hello';
-    document.getElementById('___loader').style.display = 'none';
-  });
-
-};*/
 export const onInitialClientRender = () => {
   setTimeout(() => {
     document.getElementById('___loader').style.display = 'none';
