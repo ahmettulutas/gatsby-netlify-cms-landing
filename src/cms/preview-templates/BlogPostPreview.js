02fc172c418f82199c19e9050165b466f22b7829
ahmetulutas <ahmet.ulutas@ecozum.com>
2023-02-03 10:57:20 +0300

    bugfix: blogpost preview bugfix

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BlogPostTemplate } from '../../templates/blog-post';
 
-const BlogPostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags']);
+const BlogPostPreview = ({ entry, widgetFor, getAsset }) => {
+  const tags = entry.getIn(['data', 'tags']),
+    data = entry.getIn(['data']).toJS();
   return (
     <BlogPostTemplate
       content={widgetFor('body')}
       description={entry.getIn(['data', 'description'])}
       tags={tags && tags.toJS()}
       title={entry.getIn(['data', 'title'])}
+      image={getAsset(data.image)}
     />
   );
 };
